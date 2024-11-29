12a688695cb197f6a0b7fc9cee9f5d585d81396a
nitahai <hi.nitah@hotmail.com>
2024-11-29 15:18:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post(`/webhook/${token}`, async (req, res) => {
 
     // Jika pesan teks adalah "/informasi"
     if (update.message.text === '/informasi') {
-      await sendMessage(chatId, 'Bot Nitah ini dirancang untuk membantu memproses gambar soal pelajaran sekolah kamu dan mencari jawaban dengan cepat. Cukup kirimkan gambar soalmu, dan Nitah akan memperoses untuk memberikan jawaban yang cepat dan tepat!');
+      await sendMessage(chatId, 'Bot Nitah ini dirancang untuk membantu memproses gambar soal pelajaran sekolah-kamu dan mencari jawaban dengan cepat. Cukup kirimkan gambar soalmu, dan Nitah akan memperoses untuk memberikan jawaban yang cepat dan tepat!');
     }
 
     // Jika pesan teks adalah "/tentang"
